93f3fbd44bc617203f76d5979cfeb1521ba802f2
Konstantin <92136415+JoJoBizarrio@users.noreply.github.com>
2024-12-06 17:46:28 +0700

    added sendMessage and sendFileByUrl, along with minor fixes.

--- script.js
+++ script.js
@@ -39,3 +39,42 @@ const result = document.getElementById("result");
             console.log(`Problems getting instance state: ${error}`);
         })
     })
+
+    document.getElementById("sendMessageButton").addEventListener("click", function () {
+        const apiTokenInstance = document.getElementById("apiTokenInstance").value;
+        const idInstance = document.getElementById("idInstance").value;
+        const chatId = `${document.getElementById("phoneReceivingMessages").value}@c.us`;
+        const message = document.getElementById("message").value;
+        const result = document.querySelector("#result");
+
+        const restAPI = whatsAppClient.restAPI({
+            idInstance: idInstance,
+            apiTokenInstance: apiTokenInstance
+        });
+
+        restAPI.message.sendMessage(chatId, null, message).then((data) => {
+                result.textContent = getConvertedText(data);
+            }).catch((error) => {
+                console.error("Error:", error);
+            });
+    });
+
+    document.getElementById("sendFileByUrlButton").addEventListener("click", () => {
+        const apiTokenInstance = document.getElementById("apiTokenInstance").value;
+        const idInstance = document.getElementById("idInstance").value
+        const chatId = `${document.getElementById("phoneReceivingFile").value}@c.us`;
+        const url = document.getElementById('fileUrl').value;
+        const fileName = document.getElementById('fileName').value;
+        const result = document.querySelector("#result");
+
+        const restAPI = whatsAppClient.restAPI({
+            idInstance: idInstance,
+            apiTokenInstance: apiTokenInstance
+        });
+
+        restAPI.file.sendFileByUrl(chatId, null, url, fileName).then((data) => {
+            result.textContent = getConvertedText(data);
+        }).catch((error) => {
+            console.error("Error:", error);
+        })
+    })
